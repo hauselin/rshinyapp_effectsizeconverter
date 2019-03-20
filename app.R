cf68f6350e17e1b6c7cc3507eb9f5714b23742c3
Hause Lin <hauselin@gmail.com>
2019-03-20 21:37:42 +0100

    minor edits

--- app.R
+++ app.R
@@ -7,6 +7,6 @@ server <- function(input, output, session) {}
 ui <- fluidPage(
     # tags$head(includeScript("google-analytics.js")),
     h4("The app has moved. This URL will stop working soon..."),
-    h4("Bookmark/save this new site:", a("http://escal.site", href="http://escal.site"))
+    h4("Visit and bookmark/save the new site:", a("http://escal.site", href="http://escal.site"))
     )
 shinyApp(ui = ui, server = server)
